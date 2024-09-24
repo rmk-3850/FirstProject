5cbbc878c939b72b3b823fcee35c8bce50440bef
sumin <sumin750@gmail.com>
2024-09-24 19:16:40 +0900

    등록기간 조회, 페이징 기능

--- src/main/java/bean/CustomerDAO.java
+++ src/main/java/bean/CustomerDAO.java
@@ -93,6 +93,87 @@ public class CustomerDAO {
  		return list;
  	}
     
+ // 새로운 메소드 추가 (customerDTOList 검색)
+    public List<CustomerDTO> getCustomerDTOList(String keyField, String keyWord) {
+        String sql = null;
+
+        if (keyWord == null || keyWord.isEmpty()) {
+            sql = "SELECT * FROM cus";
+        } else {
+            sql = "SELECT * FROM cus WHERE " + keyField + " LIKE ?";
+        }
+
+        ArrayList<CustomerDTO> customerList = new ArrayList<>();
+
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(sql);
+
+            if (keyWord != null && !keyWord.isEmpty()) {
+                statement.setString(1, "%" + keyWord + "%");
+            }
+
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                CustomerDTO customer = new CustomerDTO();
+                customer.setCus_id(resultSet.getInt("cus_id"));
+                customer.setCus_name(resultSet.getString("cus_name"));
+                customer.setCus_gender(resultSet.getString("cus_gender"));
+                customer.setCus_ph(resultSet.getString("cus_ph"));
+                customer.setCus_mail(resultSet.getString("cus_mail"));
+                customer.setCus_reg(resultSet.getString("cus_reg"));
+                customer.setCus_rank(resultSet.getString("cus_rank"));
+                customer.setCus_note(resultSet.getString("cus_note"));
+
+                customerList.add(customer);
+            }
+        } catch (SQLException e) {
+            System.out.println("[getCustomerDTOList] Message : " + e.getMessage());
+            System.out.println("[getCustomerDTOList] Class   : " + e.getClass().getSimpleName());
+        } finally {
+            freeConnection();
+        }
+        return customerList;
+    }
+    
+// 등록 기간별 회원 조회
+    public List<CustomerDTO> getCustomerByregdate(String startDate, String endDate) {
+    	String sql = "SELECT * FROM cus WHERE cus_reg BETWEEN ? AND ?";
+    	ArrayList<CustomerDTO> customerList = new ArrayList<>();
+        
+        try {
+        	connection = dataSource.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, startDate);
+            statement.setString(2, endDate);
+
+
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                CustomerDTO customer = new CustomerDTO();
+                customer.setCus_id(resultSet.getInt("cus_id"));
+                customer.setCus_name(resultSet.getString("cus_name"));
+                customer.setCus_gender(resultSet.getString("cus_gender"));
+                customer.setCus_ph(resultSet.getString("cus_ph"));
+                customer.setCus_mail(resultSet.getString("cus_mail"));
+                customer.setCus_reg(resultSet.getString("cus_reg"));
+                customer.setCus_rank(resultSet.getString("cus_rank"));
+                customer.setCus_note(resultSet.getString("cus_note"));
+
+                customerList.add(customer);
+            }
+        } catch (SQLException e) {
+            System.out.println("[getCustomerByregdate] Message : " + e.getMessage());
+            System.out.println("[getCustomerByregdate] Class   : " + e.getClass().getSimpleName());
+        } finally {
+            freeConnection();
+        }
+        return customerList;
+    }
+    
+    
     //reservationPost.jsp 
  // 예약자명 조회
     public List<String> getAllCustomerNames() throws SQLException {
